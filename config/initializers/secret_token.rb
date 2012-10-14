10e9206f7f41ab7d0f75ef6493a2bd52180c3ea0
Kevin Gomes <kevingomes17@gmail.com>
2012-10-14 18:57:57 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFBApp::Application.config.secret_token = 'd3a685f07fd4de54f60617cf08f9bd543d33b286c90b1bf627c55e8d6fe3694028c94c8fdd5be65b30b9da6b0457337b6e02030c5404c35a6ecbcf1093b6624f'
