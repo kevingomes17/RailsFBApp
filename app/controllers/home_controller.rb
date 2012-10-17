8ea47e8388c5fed9f0c66fbfa4d2f0b9ffdc1bcb
Kevin Gomes <kevingomes17@gmail.com>
2012-10-17 08:51:21 -0500

    Fetching user info from email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,48 +4,59 @@ class HomeController < ApplicationController
   protect_from_forgery
 
   def index
-      #graph = Koala::Facebook::GraphAPI.new(@facebook_cookies["access_token"])
-      #@likes = graph.get_connections("me", "likes")
+    #graph = Koala::Facebook::GraphAPI.new(@facebook_cookies["access_token"])
+    #@likes = graph.get_connections("me", "likes")
 
-      session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
-      		      @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream,read_friendlists,user_location,email")
-		      puts session.to_s + "<<< session"
+    session[:oauth] = Koala::Facebook::OAuth.new(APP_ID, APP_SECRET, SITE_URL + '/home/callback')
+    @auth_url =  session[:oauth].url_for_oauth_code(:permissions=>"read_stream,read_friendlists,user_location,email")
+    puts session.to_s + "<<< session"
 
-      respond_to do |format|
-                  format.html {  }
-      end
+    respond_to do |format|
+      format.html {  }
+    end
   end
-
-
  
   def callback
-        if params[:code]
-                # acknowledge code and get access token from FB
-                  session[:access_token] = session[:oauth].get_access_token(params[:code])
-                end
+		if params[:code]
+      # acknowledge code and get access token from FB
+      session[:access_token] = session[:oauth].get_access_token(params[:code])
+    end
 
-                 # auth established, now do a graph call:
+    # auth established, now do a graph call:
 
-		   @api = Koala::Facebook::API.new(session[:access_token])
-		   	begin
-			    @me = @api.get_object("/me")
-                     	@graph_data = @api.get_object("/me/friends")
-				    rescue Exception=>ex
-						puts ex.message
-						     end
+    @api = Koala::Facebook::API.new(session[:access_token])
+    begin
+      #@user = @api.search("FBFriend@LiveWat.ch", {:type=>"user"})
+      #@user = @api.search("koala")
+      #@me = @api.get_object("/me")
+      #@graph_data = @api.get_object("/me/friends")
+      
+      @users = FbUser.all
+      #@users = (current_user.blank? ? User.all : User.find(:all, :conditions => ["id != ?", current_user.id]))
+      @users.each do |user|
+        @fbUserObj = @api.search(user["email"], {:type=>"user"})
+        user.fbid = @fbUserObj[0]["id"] if (@fbUserObj != nil)
+        user.save!
+      end
+      
+=begin
+        cur_user = FbUser.find_or_initialize_by_fbid(@me["id"])
+        cur_user.email = @me["email"]
+        cur_user.name = @me["name"]
+        cur_user.save!
 
-
-    cur_user = FbUser.find_or_initialize_by_fbid(@me["id"])
-    cur_user.email = @me["email"]
-    cur_user.name = @me["name"]
-    cur_user.save!
-
-    @graph_data.each do |friend|
-    	c = FbUser.find_or_initialize_by_fbid(:name=>friend["name"], :fbid => friend["id"])
-    	c.save!
-    end						     
-                respond_to do |format|
-                 format.html {   }
-		 	     end
-			     end
+        @graph_data.each do |friend|
+          c = FbUser.find_or_initialize_by_fbid(:name=>friend["name"], :fbid => friend["id"])
+          c.save!
+        end
+=end	
+      
+    rescue Exception=>ex
+      puts ex.message
+    end
+    
+    respond_to do |format|
+      format.html {   }
+    end
+  end
 end
